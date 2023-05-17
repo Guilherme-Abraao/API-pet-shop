88e144fe5cabda7eb9284815d89979d6e94c7016
Guilherme-Abraao <abraaosilva.guilherme@gmail.com>
2023-05-17 10:39:09 -0300

    Estruturando Front-End, criando components footer, header, pages e singular-components

--- Front_End/src/app/app.module.ts
+++ Front_End/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer/footer.component';
+import { FormCadastroUsuarioComponent } from './components/singular-components/form-cadastro-usuario/form-cadastro-usuario.component';
+import { MensagensSistemaComponent } from './components/singular-components/mensagens-sistema/mensagens-sistema.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { CadastroUsuarioComponent } from './components/pages/cadastro-usuario/cadastro-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    FormCadastroUsuarioComponent,
+    MensagensSistemaComponent,
+    LoginComponent,
+    CadastroUsuarioComponent
   ],
   imports: [
     BrowserModule,
