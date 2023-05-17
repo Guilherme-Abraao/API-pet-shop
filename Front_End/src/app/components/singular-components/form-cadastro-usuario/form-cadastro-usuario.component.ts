cb695900f1d4064f509a652dacab4d3c39537c4a
Guilherme-Abraao <abraaosilva.guilherme@gmail.com>
2023-05-17 15:55:08 -0300

    Fazendo validação dos campos do userForm

--- Front_End/src/app/components/singular-components/form-cadastro-usuario/form-cadastro-usuario.component.ts
+++ Front_End/src/app/components/singular-components/form-cadastro-usuario/form-cadastro-usuario.component.ts
@@ -1,17 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { CpfService } from 'src/app/services/validacao-cpf.service';
 
 @Component({
   selector: 'app-form-cadastro-usuario',
   templateUrl: './form-cadastro-usuario.component.html',
-  styleUrls: ['./form-cadastro-usuario.component.css']
+  styleUrls: ['./form-cadastro-usuario.component.css'],
 })
-export class FormCadastroUsuarioComponent implements OnInit{
-  faTimes = faTimes;
+export class FormCadastroUsuarioComponent implements OnInit {
+  @Input() btnText!: string;
+  @Input() titulo!: string;
 
-  constructor(){ }
+  userForm!: FormGroup;
+  faTimes = faTimes;
+  cpfValido: boolean = false;
+
+  constructor(private cpfService: CpfService) {}
+
+  /* Inicialização do formulário */
   ngOnInit(): void {
-    
+    this.userForm = new FormGroup({
+      id: new FormControl(''),
+      role: new FormControl(''),
+      nome: new FormControl('', [Validators.required]),
+      cpf: new FormControl('', [Validators.required]),
+      dataNascimento: new FormControl('', [Validators.required]),
+      telefone: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required]),
+      senha: new FormControl('', [Validators.required]),
+      confirmacaoSenha: new FormControl('', [Validators.required]),
+    });
   }
 
+  /* Errado e imcompleto*/
+  verificarCpf(): boolean {
+    const cpfValue = this.userForm.get('cpf')!.value;
+    return !this.cpfService.validarCpf(cpfValue);
+  }
+
+  /* GETs dos itens do formulário */
+
+  get nome() {
+    return this.userForm.get('nome')!;
+  }
+  get cpf() {
+    return this.userForm.get('cpf')!;
+  }
+  get dataNascimento() {
+    return this.userForm.get('dataNascimento')!;
+  }
+  get telefone() {
+    return this.userForm.get('telefone')!;
+  }
+  get email() {
+    return this.userForm.get('email')!;
+  }
+  get senha() {
+    return this.userForm.get('senha')!;
+  }
+  get confirmacaoSenha() {
+    return this.userForm.get('confirmacaoSenha')!;
+  }
+
+  submit() {
+    if (this.userForm.invalid) {
+      return;
+    }
+    return 1;
+  }
 }
---
+++ Front_End/src/app/services/validacao-cpf.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CpfService {
+  constructor() {}
+
+  validarCpf(cpf: string): boolean {
+    // Remova qualquer formatação do CPF (como pontos e traços)
+    cpf = cpf.replace(/\D/g, '');
+
+    // Verifique se o CPF possui 11 dígitos
+    if (cpf.length !== 11) {
+      return false;
+    }
+
+    // Verifique se todos os dígitos são iguais
+    if (/^(\d)\1+$/.test(cpf)) {
+      return false;
+    }
+
+    // Validação dos dígitos verificadores
+    let sum = 0;
+    let remainder: number;
+
+    for (let i = 1; i <= 9; i++) {
+      sum += parseInt(cpf.substring(i - 1, i)) * (11 - i);
+    }
+
+    remainder = (sum * 10) % 11;
+
+    if (remainder === 10 || remainder === 11) {
+      remainder = 0;
+    }
+
+    if (remainder !== parseInt(cpf.substring(9, 10))) {
+      return false;
+    }
+
+    sum = 0;
+
+    for (let i = 1; i <= 10; i++) {
+      sum += parseInt(cpf.substring(i - 1, i)) * (12 - i);
+    }
+
+    remainder = (sum * 10) % 11;
+
+    if (remainder === 10 || remainder === 11) {
+      remainder = 0;
+    }
+
+    if (remainder !== parseInt(cpf.substring(10, 11))) {
+      return false;
+    }
+
+    return true;
+  }
+}
