8a4ba2a61d5f4896bc4987e88fdd5e80b4d7e3fd
Wendel-Fl <wendelfl.dev@gmail.com>
2023-06-16 16:36:03 -0300

    :white_check_mark: Descomentando testes

--- Back_End/petShop/src/test/java/com/example/petshop/repository/AdministradorRepositoryTest.java
+++ Back_End/petShop/src/test/java/com/example/petshop/repository/AdministradorRepositoryTest.java
@@ -1,53 +1,56 @@
-//package com.example.petshop.repository;
-//
-//import com.example.petshop.base.Funcionario;
-//import org.assertj.core.api.Assertions;
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//
-//import java.time.LocalDate;
-//import java.time.Month;
-//import java.util.Optional;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//@DataJpaTest
-//class AdministradorRepositoryTest {
-//
-//    @Autowired
-//    private FuncionarioRepository underTest;
-//
-//    @AfterEach
-//    void tearDown() {
-//        underTest.deleteAll();
-//    }
-//
-//    @Test
-//    void findAdministradorByEmail() {
-//        String email = "tony.stark@gmail.com";
-//
-//        Funcionario funcionario = new Funcionario(
-//                "Tony",
-//                "tony.stark@gmail.com",
-//                "55693710017",
-//                "(62) 28362-4799",
-//                "8r0A8nTjHx",
-//                LocalDate.of(1980, Month.MARCH, 17),
-//                "gerente",
-//                10000.00
-//        );
-//
-//        underTest.save(funcionario);
-//
-//        //when
-//        Optional<Funcionario> expected = underTest.findFuncionarioByEmail(email);
-//
-//
-//
-//        //then
-//        Assertions.assertThat(expected).isPresent();
-//
-//    }
-//}
+package com.example.petshop.repository;
+
+import com.example.petshop.base.Funcionario;
+import com.example.petshop.base.Role;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Optional;
+
+import static com.example.petshop.base.Role.FUNCIONARIO;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class AdministradorRepositoryTest {
+
+    @Autowired
+    private FuncionarioRepository underTest;
+
+    @AfterEach
+    void tearDown() {
+        underTest.deleteAll();
+    }
+
+    @Test
+    void findAdministradorByEmail() {
+        String email = "tony.stark@gmail.com";
+
+        Funcionario funcionario = new Funcionario(
+                "Tony",
+                "tony.stark@gmail.com",
+                "55693710017",
+                "(62) 28362-4799",
+                "8r0A8nTjHx",
+                LocalDate.of(1980, Month.MARCH, 17),
+                FUNCIONARIO,
+                "gerente",
+                10000.00
+        );
+
+        underTest.save(funcionario);
+
+        //when
+        Optional<Funcionario> expected = underTest.findFuncionarioByEmail(email);
+
+
+
+        //then
+        Assertions.assertThat(expected).isPresent();
+
+    }
+}
--- Back_End/petShop/src/test/java/com/example/petshop/repository/FuncionarioRepositoryTest.java
+++ Back_End/petShop/src/test/java/com/example/petshop/repository/FuncionarioRepositoryTest.java
@@ -1,61 +1,64 @@
-//package com.example.petshop.repository;
-//
-//import net.bytebuddy.asm.Advice;
-//import org.assertj.core.api.Assertions;
-//import org.junit.jupiter.api.AfterEach;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Import;
-//
-//
-//import com.example.petshop.base.Funcionario;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//
-//import java.time.LocalDate;
-//import java.time.Month;
-//import java.util.Date;
-//import java.util.Optional;
-//
-//import static org.hamcrest.MatcherAssert.assertThat;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-//@DataJpaTest
-//class FuncionarioRepositoryTest {
-//
-//    @Autowired
-//    private FuncionarioRepository underTest;
-//
-//    @AfterEach
-//    void tearDown() {
-//        underTest.deleteAll();
-//    }
-//
-//    @Test
-//    void TestfindFuncionarioByEmail() {
-//        //given
-//
-//        String email = "Catatas@gmail.com";
-//
-//        Funcionario funcionario = new Funcionario(
-//                "Catatau",
-//                "Catatas@gmail.com",
-//                "12332112332",
-//                "6290028922",
-//                "Senha123",
-//                LocalDate.of(1969, Month.APRIL,20),
-//                "Banhista",
-//                2000.00
-//        );
-//
-//        underTest.save(funcionario);
-//
-//        //when
-//        Optional<Funcionario> expected = underTest.findFuncionarioByEmail(email);
-//
-//
-//
-//        //then
-//        Assertions.assertThat(expected).isPresent();
-//    }
-//}
+package com.example.petshop.repository;
+
+import com.example.petshop.base.Role;
+import net.bytebuddy.asm.Advice;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+
+import com.example.petshop.base.Funcionario;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Date;
+import java.util.Optional;
+
+import static com.example.petshop.base.Role.FUNCIONARIO;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DataJpaTest
+class FuncionarioRepositoryTest {
+
+    @Autowired
+    private FuncionarioRepository underTest;
+
+    @AfterEach
+    void tearDown() {
+        underTest.deleteAll();
+    }
+
+    @Test
+    void TestfindFuncionarioByEmail() {
+        //given
+
+        String email = "Catatas@gmail.com";
+
+        Funcionario funcionario = new Funcionario(
+                "Catatau",
+                "Catatas@gmail.com",
+                "12332112332",
+                "6290028922",
+                "Senha123",
+                LocalDate.of(1969, Month.APRIL,20),
+                FUNCIONARIO,
+                "Banhista",
+                2000.00
+        );
+
+        underTest.save(funcionario);
+
+        //when
+        Optional<Funcionario> expected = underTest.findFuncionarioByEmail(email);
+
+
+
+        //then
+        Assertions.assertThat(expected).isPresent();
+    }
+}
