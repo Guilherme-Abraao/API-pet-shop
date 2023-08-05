9853a664266cd11a882ac62ac939bdd1d31046e3
Wendel-Fl <wendelfl.dev@gmail.com>
2023-08-05 00:42:48 -0300

    Resolvendo construtores e convertendo AgendamentoRequest para um ArrayList

--- Back_End/petShop/src/main/java/com/example/petshop/base/Administrador.java
+++ Back_End/petShop/src/main/java/com/example/petshop/base/Administrador.java
@@ -1,24 +1,28 @@
 package com.example.petshop.base;
 
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+import static com.example.petshop.base.Role.ADMINISTRADOR;
+
 @NoArgsConstructor
 @Entity
 public class Administrador extends Funcionario{
-    public Administrador(String nome,
-                         String email,
-                         String CPF,
-                         String telefone,
-                         String senha,
-                         LocalDate dataNascimento,
-                         Role role,
-                         Cargo cargo,
-                         Double salario) {
-        super(nome, email, CPF, telefone, senha, dataNascimento, role, cargo, salario);
+    public Administrador(
+            String nome,
+            String email,
+            String cpf,
+            String telefone,
+            String senha,
+            LocalDate dataNascimento,
+            Cargo cargo,
+            @NotNull Double salario
+    ) {
+        super(nome, email, cpf, telefone, senha, dataNascimento, ADMINISTRADOR, cargo, salario);
     }
 }
